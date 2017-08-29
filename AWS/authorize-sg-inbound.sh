06a52ec6510993b004e8e3ebcac4d26c98cb2274
Wang Xiaodong <wang_0120@163.com>
2017-08-30 07:49:18 +0800

    update authorize sg script

--- AWS/authorize-sg-inbound.sh
+++ AWS/authorize-sg-inbound.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
-case $1 in
-	"ap-southeast-1")
-		REGION="ap-southeast-1"
-		;;
-	*)
-		REGION="us-west-1"
-		;;
-esac
+while [ "$1" ] ; do
+    case "$1" in
+        "-r" | "--region" )
+            REGION="$2"
+            shift 2
+            ;;
+        "--cidr" )
+            CIDR="$2"
+            shift 2
+            ;;
+        "-t" | "--type" )
+            TYPE="$2"
+            shift 2
+            ;;
+        *)
+            echo "[ERROR] invalid command: $1"
+            exit 1
+            ;;
+    esac
+done
 
-echo "region:$REGION"
+if [[ "$REGION" == "" ]]; then
+    REGION="us-west-1"
+fi
+
+# if no cidr, set the current IP as default IP
+if [[ "$CIDR" == "" ]]; then
+    MYIP=$(curl -XGET "http://checkip.amazonaws.com")
+    CIDR=${MYIP}/32
+fi
+
+echo "region: $REGION, my CIDR: $CIDR, type: $TYPE "
 
 SGLIST=$(aws ec2 describe-security-groups --query 'SecurityGroups[*].{ID:GroupId}' --region $REGION --output text)
-MYIP=$(curl -XGET "http://checkip.amazonaws.com")
-MYCIDR=${MYIP}/32
-echo "my CIDR: $MYCIDR"
-
 for SG in $SGLIST
 do
-	echo "add ssh for $SG,$MYCIDR,$REGION"
-	aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $MYCIDR --region $REGION
-# 	aws ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $MYCIDR --region $REGION
+    if [[ $TYPE == "authorize" ]]; then
+	    echo "authorize ssh for $SG,$CIDR,$REGION"
+	    #	aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $CIDR --region $REGION
+	elif [[ $TYPE == "revoke" ]]; then
+        echo "revoke ssh for $SG,$CIDR,$REGION"
+        #	aws ec2 revoke-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr $CIDR --region $REGION
+    else
+        echo "invalid type: should be [authorize | revoke]"
+        exit 1
+     fi
 done
