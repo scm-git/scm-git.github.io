8988d8b1443e02d807d41fe4c62629f1f821ec83
王孝东 <wang_0120@163.com>
2019-07-21 21:41:37 +0800

    update cv

---
+++ resume/wxd-normal.js
@@ -0,0 +1,8 @@
+﻿(function (jsPDFAPI) {
+var font = '5Lit5paHCg==';
+var callAddFont = function () {
+this.addFileToVFS('wxd-normal.ttf', font);
+this.addFont('wxd-normal.ttf', 'wxd', 'normal');
+};
+jsPDFAPI.events.push(['addFonts', callAddFont])
+ })(jsPDF.API);
