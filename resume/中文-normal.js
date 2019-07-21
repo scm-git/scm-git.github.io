798d5ae9abba3b76ad50d58c430dad684a097101
王孝东 <wang_0120@163.com>
2019-07-21 21:27:00 +0800

    add pdf font

---
+++ resume/中文-normal.js
@@ -0,0 +1,8 @@
+﻿(function (jsPDFAPI) {
+var font = '5Lit5paHCg==';
+var callAddFont = function () {
+this.addFileToVFS('中文-normal.ttf', font);
+this.addFont('中文-normal.ttf', '中文', 'normal');
+};
+jsPDFAPI.events.push(['addFonts', callAddFont])
+ })(jsPDF.API);
